81e61ed92b623c84bc9efb5481dff8eefbbb1afe
neha <na2542@columbia.edu>
2014-09-14 16:34:16 -0400

    Indentation improved

--- shell/shell.c
+++ shell/shell.c
@@ -57,7 +57,6 @@ char* get_input() {
 		printf("Memory Overflow");
 		return NULL;
 	} else {
-
 		str = temp_malloc;
 	}
 	fgets(str, mem, stdin);
@@ -67,10 +66,7 @@ char* get_input() {
 		temp_realloc = realloc(str, mem);
 		if (temp_realloc == NULL) {
 			printf("Memory Overflow");
-			while (next != '\n' && next != EOF) {
-				next = getchar();
-			}
-
+			while ((next = getchar()) != '\n' && next != EOF);
 			return NULL;
 		} else {
 			str = temp_realloc;
@@ -89,25 +85,16 @@ char* handle_commands(char *cmd) {
 	char *token, *str, *token_found;
 	int flag = FALSE;
 	char *temp_malloc = (char *) malloc(strlen(path) + 1);
-	if (temp_malloc == NULL) {
-		printf("Memory Overflow");
-		return NULL;
-	}
+	if (temp_malloc == NULL) {return NULL;}
 	str = temp_malloc;
 	strcpy(str, path);
 	token = strtok(str, ":");
 	while (token != NULL) {
 		if (scan_directory(token, cmd) == TRUE) {
-
 			char *temp = (char *) malloc(strlen(token) + 1);
-			if (temp == NULL) {
-				printf("Memory Overflow");
-				return NULL;
-			}
+			if (temp == NULL) {return NULL;}
 			token_found = temp;
 			strcpy(token_found, token);
-			printf("token: %s\n", token);
-			printf("token_found: %s\n", token_found);
 			flag = TRUE;
 		}
 		token = strtok(NULL, ":");
@@ -133,13 +120,10 @@ int scan_directory(char *dir, char *target) {
 	int n;
 	struct dirent **entry;
 	n = scandir(dir, &entry, 0, alphasort);
-	if (n < 0) {
-		return FALSE;
-	} else {
+	if (n < 0) {return FALSE;
+	}else {
 		while (n--) {
-			if (strcmp(entry[n]-> d_name, target) == 0) {
-				flag = TRUE;
-			}
+			if (strcmp(entry[n]-> d_name, target) == 0) flag = TRUE;
 			free(entry[n]);
 		}
 		free(entry);
@@ -153,32 +137,47 @@ void handle_path(int cmd_length, char *input[]){
 	char *path_token="";
 	if (cmd_length == 1) {
 		printf("%s\n", path);
-	} else if (cmd_length >= 3) {
-		if (strcmp(input[1], "+") == 0) {
-			printf("path argument: %s\n", input[2]);
-			if (strlen(path) > 0) {
-				path = concat(path, ":");
+	} else if (cmd_length >= 3 && (strcmp(input[1], "+") == 0)) {
+		if (strlen(path) > 0) path = concat(path, ":");
+		path = concat(path, input[2]);
+	}else if (cmd_length >= 3 && strcmp(input[1], "-") == 0 && strcmp(path, "") != 0) {
+		char *b = "";
+		path_token = strtok(path, ":");
+		while (path_token) {
+			if (strcmp(path_token, input[2]) != 0) {
+				if (strlen(b) > 0) b = concat(b, ":");
+				b = concat(b, path_token);
 			}
-			path = concat(path, input[2]);
+			path_token = strtok(NULL, ":");
 		}
-		if (strcmp(input[1], "-") == 0 && strcmp(path, "") != 0) {
-			char *b = "";
-			path_token = strtok(path, ":");
-			while (path_token) {
-				if (strcmp(path_token, input[2]) != 0) {
-					if (strlen(b) > 0) {
-						b = concat(b, ":");
-					}
-					b = concat(b, path_token);
-				}
-				path_token = strtok(NULL, ":");
-			}
-			if(strcmp(path, "") != 0){
-				free(path);
-				path = NULL;
-			}
-			path = b;
+		if(strcmp(path, "") != 0){
+			free(path);
+			path = NULL;
 		}
+		path = b;
+	}
+
+}
+
+/*Executes commands using fork, wait and execv*/
+void execute_commands(pid_t pid, char *input, char *cmd_path, int len){
+	if (pid < 0) {
+		break;
+	}
+	if(cmd_path == NULL){
+		printf("error : Command not found\n");
+	}
+	if (pid > 0) {
+		int child_status;
+		wait(&child_status);
+	}
+	if (pid == 0 && cmd_path != NULL) {
+		cmd_path = concat(cmd_path, "/");
+		cmd_path = concat(cmd_path, input[0]);
+		input[len] = NULL;
+		execv(cmd_path, input);
+		printf("error : %s\n", strerror(errno));
+		exit(0);
 	}
 }
 
@@ -200,40 +199,37 @@ int main() {
 			cmd[i++] = token;
 			token = strtok(NULL, " \t");
 		}
-		if (strcmp(cmd[0], "exit") == 0) {
+		if(i<=0){
+			continue;
+		} else if (strcmp(cmd[0], "exit") == 0) {
 			printf("EXITING shell\n");
+			free(str);
+			str = NULL;
 			exit(0);
 		} else if (strcmp(cmd[0], "cd") == 0) {
 			printf("==>");
-			if (chdir(cmd[1]) < 0) {
-				printf("error : %s\n", strerror(errno));
-			}
+			if (chdir(cmd[1]) < 0) {printf("error : %s\n", strerror(errno));}
 		} else if (i >= 1 && strcmp(cmd[0], "path") == 0) {
 			handle_path(i, cmd);
 		}else {
-			if (i >= 1) {
-				cmd_path = handle_commands(cmd[0]);
-				pid = fork();
-				if (pid < 0) {
-					break;
-				}
-				if(cmd_path == NULL){
-					printf("error : Command not found\n");
-				}
-				if (pid > 0) {
-					int child_status;
-					wait(&child_status);
-				}
-				if (pid == 0 && cmd_path != NULL) {
-					cmd_path = concat(cmd_path, "/");
-					cmd_path = concat(cmd_path, cmd[0]);
-					cmd[i] = NULL;
-					printf("cmd_path: %s\n", cmd_path);
-					execv(cmd_path, cmd);
-					printf("error : %s\n", strerror(errno));
-					exit(0);
-				}
+			cmd_path = handle_commands(cmd[0]);
+			pid = fork();
+			execute_commands(pid, cmd, cmd_path, i);
+			/*if (pid < 0) {break;}
+			if(cmd_path == NULL){printf("error : Command not found\n");}
+			if (pid > 0) {
+				int child_status;
+				wait(&child_status);
 			}
+			if (pid == 0 && cmd_path != NULL) {
+				cmd_path = concat(cmd_path, "/");
+				cmd_path = concat(cmd_path, cmd[0]);
+				cmd[i] = NULL;
+				execv(cmd_path, cmd);
+				printf("error : %s\n", strerror(errno));
+				exit(0);
+			}*/
+
 		}
 		free(str);
 		str = NULL;
