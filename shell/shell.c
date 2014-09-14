a3f0c24725ebf29e457d84859e476efb9baeee54
neha <na2542@columbia.edu>
2014-09-14 17:42:01 -0400

    checkpatch bugs removed

--- shell/shell.c
+++ shell/shell.c
@@ -17,16 +17,19 @@ pid_t wait(int *status);
 pid_t fork(void);
 int execv(const char *path, char *const argv[]);
 
-/* Path variable to be used to maintain path of the executables required to run the commands*/
+/* Path variable to be used to maintain path of the
+ * executables required to run the commands*/
 char *path = "";
 
 /*Function to concatenate two strings*/
-char* concat(char *s1, char *s2) {
+char *concat(char *s1, char *s2)
+{
 	if (s1 == NULL || s2 == NULL || (strcmp(s2, "") == 0)) {
 		printf("Invalid Strings");
 		return s1;
 	}
-	char *temp_malloc = (char *) malloc(strlen(s1) + strlen(s2) + 1), *result;
+	char *temp_malloc = (char *) malloc(strlen(s1)
+			+ strlen(s2) + 1), *result;
 
 	if (temp_malloc == NULL) {
 		printf("Memory Overflow");
@@ -49,16 +52,18 @@ char* concat(char *s1, char *s2) {
 }
 
 /* Getting input from the user*/
-char* get_input() {
+char *get_input(void)
+{
 	int mem = 128;
-	char *temp_malloc = (char*) malloc(mem);
+	char *temp_malloc = (char *) malloc(mem);
 	char *str, next, *temp_realloc;
+
 	if (temp_malloc == NULL) {
 		printf("Memory Overflow");
 		return NULL;
-	} else {
-		str = temp_malloc;
 	}
+	str = temp_malloc;
+
 	fgets(str, mem, stdin);
 
 	while (str[strlen(str) - 1] != '\n') {
@@ -66,64 +71,73 @@ char* get_input() {
 		temp_realloc = realloc(str, mem);
 		if (temp_realloc == NULL) {
 			printf("Memory Overflow");
-			while ((next = getchar()) != '\n' && next != EOF);
+			while ((next = getchar()) != '\n' && next != EOF)
+				;
 			return NULL;
-		} else {
-			str = temp_realloc;
-			fgets(str + mem / 2 - 1, mem / 2 + 1, stdin);
 		}
+		str = temp_realloc;
+		fgets(str + mem / 2 - 1, mem / 2 + 1, stdin);
 	}
 	str[strlen(str) - 1] = '\0';
 	return str;
 }
 
 /*Returns location of the executable for the given command*/
-char* handle_commands(char *cmd) {
-	if (path == NULL || strcmp(path, "") == 0) {
+char *handle_commands(char *cmd)
+{
+	if (path == NULL || strcmp(path, "") == 0)
 		return NULL;
-	}
+
 	char *token, *str, *token_found;
 	int flag = FALSE;
 	char *temp_malloc = (char *) malloc(strlen(path) + 1);
-	if (temp_malloc == NULL) {return NULL;}
+
+	if (temp_malloc == NULL)
+		return NULL;
 	str = temp_malloc;
 	strcpy(str, path);
 	token = strtok(str, ":");
 	while (token != NULL) {
 		if (scan_directory(token, cmd) == TRUE) {
 			char *temp = (char *) malloc(strlen(token) + 1);
-			if (temp == NULL) {return NULL;}
+
+			if (temp == NULL)
+				return NULL;
 			token_found = temp;
 			strcpy(token_found, token);
 			flag = TRUE;
 		}
 		token = strtok(NULL, ":");
 	}
-	if(strcmp(str, "") != 0){
+	if (strcmp(str, "") != 0) {
 		free(str);
 		str = NULL;
 	}
-	if(flag == TRUE){
+	if (flag == TRUE)
 		return token_found;
-	}else{
-		return NULL;
-	}
-	
+	return NULL;
+
 }
 
-/* Locate if given target if present in the given directory and return TRUE or FALSE */
-int scan_directory(char *dir, char *target) {
+/* Locate if given target if present in the given
+ * directory and return TRUE or FALSE */
+int scan_directory(char *dir, char *target)
+{
 	int flag = FALSE;
-	if (dir == NULL || strcmp(dir, "") == 0 || target == NULL || strcmp(target, "") == 0) {
+
+	if (dir == NULL || strcmp(dir, "") == 0 || target == NULL ||
+			strcmp(target, "") == 0)
 		return FALSE;
-	}
 	int n;
 	struct dirent **entry;
+
 	n = scandir(dir, &entry, 0, alphasort);
-	if (n < 0) {return FALSE;
-	}else {
+	if (n < 0)
+		flag = FALSE;
+	else {
 		while (n--) {
-			if (strcmp(entry[n]-> d_name, target) == 0) flag = TRUE;
+			if (strcmp(entry[n]->d_name, target) == 0)
+				flag = TRUE;
 			free(entry[n]);
 		}
 		free(entry);
@@ -133,24 +147,32 @@ int scan_directory(char *dir, char *target) {
 
 
 /* Handling path command*/
-void handle_path(int cmd_length, char *input[]){
-	char *path_token="";
-	if (cmd_length == 1) {
+void handle_path(int cmd_length, char *input[])
+{
+	char *path_token = "";
+
+	if (cmd_length == 1)
 		printf("%s\n", path);
-	} else if (cmd_length >= 3 && (strcmp(input[1], "+") == 0)) {
-		if (strlen(path) > 0) path = concat(path, ":");
+	else if (cmd_length >= 3
+			&& (strcmp(input[1], "+") == 0)) {
+		if (strlen(path) > 0)
+			path = concat(path, ":");
 		path = concat(path, input[2]);
-	}else if (cmd_length >= 3 && strcmp(input[1], "-") == 0 && strcmp(path, "") != 0) {
+	} else if (cmd_length >= 3
+			&& strcmp(input[1], "-") == 0
+			&& strcmp(path, "") != 0) {
 		char *b = "";
+
 		path_token = strtok(path, ":");
 		while (path_token) {
 			if (strcmp(path_token, input[2]) != 0) {
-				if (strlen(b) > 0) b = concat(b, ":");
+				if (strlen(b) > 0)
+					b = concat(b, ":");
 				b = concat(b, path_token);
 			}
 			path_token = strtok(NULL, ":");
 		}
-		if(strcmp(path, "") != 0){
+		if (strcmp(path, "") != 0) {
 			free(path);
 			path = NULL;
 		}
@@ -160,16 +182,17 @@ void handle_path(int cmd_length, char *input[]){
 }
 
 /*Executes commands using fork, wait and execv*/
-void execute_commands(pid_t pid, char *input[], char *cmd_path, int len){
-	if (pid < 0) {
+void execute_commands(pid_t pid, char *input[], char *cmd_path, int len)
+{
+	if (pid < 0)
 		return;
-	}
-	if(cmd_path == NULL){
+	if (cmd_path == NULL) {
 		printf("error : Command not found\n");
 		return;
 	}
 	if (pid > 0) {
 		int child_status;
+
 		wait(&child_status);
 	}
 	if (pid == 0 && cmd_path != NULL) {
@@ -182,25 +205,27 @@ void execute_commands(pid_t pid, char *input[], char *cmd_path, int len){
 	}
 }
 
-int main() {
+int main(void)
+{
 
-	char *str, *token, *cmd[100], *cmd_path="";
+	char *str, *token, *cmd[100], *cmd_path = "";
+
 	pid_t pid;
 
 	int i = 0;
+
 	while (1) {
 		i = 0;
 		printf("$");
-		str = (char*) get_input();
-		if (str == NULL) {
+		str = (char *) get_input();
+		if (str == NULL)
 			printf("error : %s\n", strerror(errno));
-		}
 		token = strtok(str, " \t");
 		while (token != NULL) {
 			cmd[i++] = token;
 			token = strtok(NULL, " \t");
 		}
-		if(i<=0){
+		if (i <= 0) {
 			continue;
 		} else if (strcmp(cmd[0], "exit") == 0) {
 			printf("EXITING shell\n");
@@ -208,33 +233,18 @@ int main() {
 			str = NULL;
 			exit(0);
 		} else if (strcmp(cmd[0], "cd") == 0) {
-			printf("==>");
-			if (chdir(cmd[1]) < 0) {printf("error : %s\n", strerror(errno));}
+			if (chdir(cmd[1]) < 0)
+				printf("error : %s\n", strerror(errno));
 		} else if (i >= 1 && strcmp(cmd[0], "path") == 0) {
 			handle_path(i, cmd);
-		}else {
+		} else {
 			cmd_path = handle_commands(cmd[0]);
 			pid = fork();
 			execute_commands(pid, cmd, cmd_path, i);
-			/*if (pid < 0) {break;}
-			if(cmd_path == NULL){printf("error : Command not found\n");}
-			if (pid > 0) {
-				int child_status;
-				wait(&child_status);
-			}
-			if (pid == 0 && cmd_path != NULL) {
-				cmd_path = concat(cmd_path, "/");
-				cmd_path = concat(cmd_path, cmd[0]);
-				cmd[i] = NULL;
-				execv(cmd_path, cmd);
-				printf("error : %s\n", strerror(errno));
-				exit(0);
-			}*/
-
 		}
 		free(str);
 		str = NULL;
 	}
-	return(0);
+	return 0;
 }
 
